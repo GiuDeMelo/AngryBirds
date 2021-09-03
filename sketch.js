efb053ed302fa7a06e52c4c61335697524983b57
Giulia <giuliavalencademelo1@gmail.com>
2021-09-03 17:40:48 -0300

    plataforma

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
+var plataform;
 var backgroundImg;
 
 function preload() {
@@ -16,7 +17,8 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,20);
+    plataform = new Ground(160,305,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -43,9 +45,13 @@ function draw(){
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
+
     box1.display();
     box2.display();
+
     ground.display();
+    plataform.display();
+
     pig1.display();
     log1.display();
 
